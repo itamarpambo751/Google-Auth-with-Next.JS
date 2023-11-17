2e261856fa9555abfcec2ba068c4e18ab0b35d8c
itamarpambo <itamarpambo751@gmail.com>
2023-11-17 18:12:32 +0100

    Create route provider

--- components/SignInButton.jsx
+++ components/SignInButton.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function SignInButton() {
     return(
-        <button className="flex items-center h-10 shadow-xl gap-4 pl-3">
+        <button className="flex items-center h-10 shadow-xl gap-2">
             <Image src="/google-logo.png" height={50} width={50} alt="google-logo" />
             <span className="h-full bg-blue-500 flex justify-center items-center px-3 text-white">Sign in With Google</span>
         </button>
